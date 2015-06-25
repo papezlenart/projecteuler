e43d18497539d020a3ab87abd5fb8dd576d6af41
Lenart <lenart.apple@gmail.com>
2015-06-25 18:06:21 +0200

    Print out test string.

--- src/ProjectEuler.java
+++ src/ProjectEuler.java
@@ -15,6 +15,8 @@ public class ProjectEuler {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Test");
     }
     
 }
